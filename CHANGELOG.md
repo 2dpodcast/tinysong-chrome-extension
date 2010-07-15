6fecf27e03ce6cef5201f222bc0fcfc37169c3b9
dhabersack <dhabersack@gmail.com>
2010-07-15 17:52:35 +1200

    Minor design changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## 1.0.1 (July 15th, 2010)
 
+* minor design changes
+* search box selected on start
 * added logo and link to http://tinysong.com/
 * search term selected after search
 
--- js/tinysong-extension.js
+++ js/tinysong-extension.js
@@ -1,30 +1,41 @@
+$(document).ready(function() {
+	container = $('#tinysong');
+
+	inputField = $('#query');
+	inputField.focus();
+});
+
 // Tinysong API URL
 var tinysongUrl = 'http://tinysong.com/s/%s?format=json';
+
+// contains results/errors
+var container;
+
+// query input field
+var inputField;
 
 // the XMLHttpRequest object
 var req;
 
 function findSongs() {
-	var query = '';
-
-	var queryElements = document.getElementById('query').value.split(' ');
-	for (element in queryElements) {
-		query += queryElements[element] + '+';
-	}
-	query = query.substring(0, query.length - 1);
+	var query = inputField.val().split(' ').join('+');
+	container.removeClass('error');
 
 	req = new XMLHttpRequest();
 	req.onload = handleResponse;
 	req.onerror = handleError;
 	req.open('GET', tinysongUrl.replace('%s', query), true);
 	req.send(null);
+
+	// give focus to search field
+	inputField.focus();
+	inputField.select();
 }
 
 // Handles parsing errors.
 function handleParsingError(error) {
-	var container = document.getElementById('tinysong');
-	container.className = 'error';
-	container.innerHTML = '<p>' + error + '</p>';
+	container.addClass('error');
+	container.html('<p>' + error + '</p>');
 }
 
 // Handles errors during the XMLHttpRequest.
@@ -34,11 +45,11 @@ function handleError() {
 
 // Handles parsing the data we got back from XMLHttpRequest.
 function handleResponse() {
-	var response = req.responseText;
+	var response = JSON.parse(req.responseText);
 
-	// TODO check for empty response in a non-stupid way
-	if (response.length == 2) {
-		handleParsingError('<strong>We didn\'t find any songs!</strong> Try searching another?');
+	if (!response.length) {
+		handleParsingError(
+			'<strong>We didn\'t find any songs!</strong> Try searching another?');
 		return;
 	}
 
@@ -46,27 +57,32 @@ function handleResponse() {
 }
 
 function buildResult(response) {
-	var container = document.getElementById('tinysong');
-	container.className = '';
-	container.innerHTML = '';
+	var resultHTML = '<ul>';
 
-	var results = JSON.parse(response);
-	for (i in results) {
-		var result = results[i];
+	for (i in response) {
+		var result = response[i];
 
 		// available attributes:
 		//   Url, SongID, SongName, ArtistID, ArtistName, AlbumID, AlbumName
 
-		container.innerHTML += '<p>' +
-				'<a href="' + result.Url + '" onclick="javascript:openUrl(\'' + result.Url + '\');">' + result.SongName + '</a>' +
-				' by <strong>' + result.ArtistName + '</strong><br>' +
-				'<em>' + result.Url + '</em>' +
-				'</p>';
+		resultHTML += '<li>' +
+				'<a href="' +
+				result.Url +
+				'" onclick="openUrl(\'' +
+				result.Url +
+				'\');">' +
+				result.SongName +
+				'</a> ' +
+				'by <strong>' +
+				result.ArtistName +
+				'</strong><br>' +
+				'<em>' +
+				result.Url +
+				'</em>' +
+				'</li>';
 	}
 
-	var inputField = document.getElementById('query');
-	inputField.focus();
-	inputField.select();
+	container.html(resultHTML + '</ul>');
 }
 
 // Open |url| in a new tab.
@@ -75,5 +91,6 @@ function openUrl(url) {
 	if (url.indexOf("http:") != 0 && url.indexOf("https:") != 0) {
 		return;
 	}
+
 	chrome.tabs.create({url: url});
 }
