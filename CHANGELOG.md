efb2b59cd7d56ffcd67c985cb85ad357019f098d
dhabersack <dhabersack@gmail.com>
2010-07-17 16:17:51 +1200

    Adding support for Grooveshark Desktop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,23 +1,30 @@
 # CHANGELOG
 
+## 1.1.0 (July 17th, 2010)
+
+* Added support for Grooveshark Desktop (gs://)
+* Added settings for default player (Desktop or Web)
+* Changed display of results, adding play button
+
+
 ## 1.0.3 (July 16th, 2010)
 
-* limited results to five songs
+* Limited results to five songs
 * Renamed to avoid confusion in Extension Gallery
 
 
 ## 1.0.2 (July 15th, 2010)
 
-* design updated to better mimic http://tinysong.com
+* Updated design to better mimic http://tinysong.com
 
 
 ## 1.0.1 (July 15th, 2010)
 
-* minor design changes
-* added logo and link to http://tinysong.com/
-* search box selected on start and after search
+* Minor design changes
+* Added logo and link to http://tinysong.com/
+* Search box selected on start and after search
 
 
-## 1.0 (July 14th, 2010)
+## 1.0.0 (July 14th, 2010)
 
-* initial release
+* Initial release
--- js/tinysong-extension.js
+++ js/tinysong-extension.js
@@ -64,31 +64,46 @@ function buildResult(response) {
 		//   Url, SongID, SongName, ArtistID, ArtistName, AlbumID, AlbumName
 
 		resultHTML += '<li>' +
-				'<a href="' +
-				result.Url +
-				'" onclick="openUrl(\'' +
-				result.Url +
-				'\');">' +
-				result.SongName +
-				'</a> ' +
-				'by <strong>' +
-				result.ArtistName +
-				'</strong><br>' +
-				'<em>' +
-				result.Url +
-				'</em>' +
+				'<a href="' + result.Url + '" class="play" onclick="javascript:openSong(\'' + result.Url + '\');">Play</a>' +
+				result.SongName + ' - ' + result.ArtistName + '<br>' +
+				'<em>' + result.Url + '</em>' +
 				'</li>';
 	}
 
 	container.html(resultHTML + '</ul>');
 }
 
+function getGsUrl(tinysongUrl) {
+	var tinysongToken = tinysongUrl.split("/")[3];
+	var gsUrl = '';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://gsuser.com/getSongTokenFromBase62/' + tinysongToken, false);
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			gsUrl = 'gs://s/~/' + xhr.responseText;
+		}
+	};
+	xhr.send();
+
+	return gsUrl;
+}
+
+// Open |url| in Grooveshark Desktop or new tab depending on settings.
+function openSong(url) {
+	if (localStorage.player == 'desktop') {
+		openUrl(getGsUrl(url));
+	} else if (localStorage.player == 'web') {
+ 		openUrl(url);
+	}
+}
+
 // Open |url| in a new tab.
 function openUrl(url) {
-	// Only allow http and https URLs.
-	if (url.indexOf("http:") != 0 && url.indexOf("https:") != 0) {
+	// Only allow http, https and gs URLs.
+	if (url.indexOf('http:') != 0 && url.indexOf('https:') != 0 && url.indexOf('gs:') != 0) {
 		return;
 	}
 
-	chrome.tabs.create({url: url});
+	chrome.tabs.create({ url: url });
 }
